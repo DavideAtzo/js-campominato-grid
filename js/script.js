1bb31808de97179b150c09b906831f81c083b33f
DavideAtzo <atzo9477@gmail.com>
2023-03-02 16:40:52 +0100

    inserimento caselle javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+/* <div class="casella">1</div> */
+
+function creaCasella(tagElement, className){
+    const element = document.createElement(tagElement);
+    element.classList.add(className);
+    return element;
+}
+
+const container = document.querySelector('.container');
+const x = 'div';
+const y = 'casella';
+
+creaCasella(x, y);
+
+for(let i = 1; i <= 100; i++){
+    const box = creaCasella(x, y);
+    container.append(box);
+}
