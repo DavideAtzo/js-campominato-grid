bc6425d9489db0919a3a074005209653b92e6577
DavideAtzo <atzo9477@gmail.com>
2023-03-03 14:40:01 +0100

    problema con calcolo caselle

--- js/script.js
+++ js/script.js
@@ -1,34 +1,65 @@
 'use strict';
-/* <div class="casella">1</div> */
 
-function creaCasella(tagElement, className, number){
+function creaCasella(tagElement, className, number) {
     const element = document.createElement(tagElement);
     element.classList.add(className);
     element.innerText = number;
     element.addEventListener(
         'click',
-        function(){
+        function () {
             console.log(number);
             element.classList.add('cambio-colore');
         }
     )
     return element;
 }
+function tabellone() {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    let selDifficolta = livDifficolta.value
+    console.log(selDifficolta);
 
-const container = document.querySelector('.container');
+    if(selDifficolta === 'difficile'){
+        cellNumber = 49;
+        for (let i = 1; i <= cellNumber; i++) {
+            const box = document.createElement('div');
+            box.classList.add('casella');
+            box.innerText = i;
+            container.append(box);
+            container.classList.add('show')
+        }
+    }else if(selDifficolta === 'normale'){
+        cellNumber = 81;
+        for (let i = 1; i <= cellNumber; i++) {
+            const box = document.createElement('div');
+            box.classList.add('casella');
+            box.innerText = i;
+            container.append(box);
+            container.classList.add('show')
+        }
+    } else{
+        cellNumber = 100;
+        for (let i = 1; i <= cellNumber; i++) {
+            const box = document.createElement('div');
+            box.classList.add('casella');
+            box.innerText = i;
+            container.append(box);
+            container.classList.add('show')
+        }
+    }
+}
 const x = 'div';
 const y = 'casella';
+let cellNumber = 0;
+const livDifficolta = document.getElementById('difficoltà');
 
 creaCasella(x, y);
-
-for(let i = 1; i <= 100; i++){
-    const box = creaCasella(x, y, i);
-    container.append(box);
-}
 const btnPlay = document.getElementById('btn-play');
 
-btnPlay.addEventListener('click',
-    function () {
-        container.classList.add('show')
-    }
-);
+btnPlay.addEventListener('click', tabellone);
+
+
+
+
+
+
