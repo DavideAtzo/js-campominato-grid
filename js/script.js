4f8ae83fc6ec6e4fc6a6848bfd1edade687890b9
DavideAtzo <atzo9477@gmail.com>
2023-03-03 16:53:04 +0100

    fix e pulizia codice

--- js/script.js
+++ js/script.js
@@ -1,70 +1,51 @@
 'use strict';
 // funziona che crea la cella 
-function creaCasella(tagElement, className, number){
+function creaCasella(tagElement, className, number) {
     const element = document.createElement(tagElement);
     element.classList.add(className);
     element.innerText = number;
+    element.addEventListener(
+        'click',
+        function () {
+            console.log(number);
+            element.classList.add('cambio-colore');
+        }
+    )
     return element;
 }
+
 // funzione che crea il tabellone di gioco 
-function Tabellone(){
+function Tabellone() {
     const container = document.querySelector('.container');
     container.innerHTML = '';
     let selDifficolta = livDifficolta.value
     console.log(selDifficolta);
-// condizione che comprende la creazione del tabellone di gioco in base alla difficolta 
-    if(selDifficolta === 'difficile'){
+    // condizione che comprende la creazione del tabellone di gioco in base alla difficolta 
+    if (selDifficolta === 'difficile') {
         cellNumber = 49;
         for (let i = 1; i <= cellNumber; i++) {
-            const box = document.createElement('div');
-            box.classList.add('casella7');
-            box.innerText = i;
-            container.append(box);
-            container.classList.add('show')
-            box.addEventListener(
-                'click',
-                function(){
-                    box.classList.add('cambio-colore');
-                }  )
+            const creaTabellone = creaCasella('div', 'casella7', i);
+            container.append(creaTabellone);
         }
-    }else if(selDifficolta === 'normale'){
+    } else if (selDifficolta === 'normale') {
         cellNumber = 81;
         for (let i = 1; i <= cellNumber; i++) {
-            const box = document.createElement('div');
-            box.classList.add('casella9');
-            box.innerText = i;
-            container.append(box);
-            container.classList.add('show')
-            box.addEventListener(
-                'click',
-                function(){
-                    box.classList.add('cambio-colore');
-                })
+            const creaTabellone = creaCasella('div', 'casella9', i);
+            container.append(creaTabellone);
         }
-    } else{
+    } else {
         cellNumber = 100;
         for (let i = 1; i <= cellNumber; i++) {
-            const box = document.createElement('div');
-            box.classList.add('casella');
-            box.innerText = i;
-            container.append(box);
-            container.classList.add('show')
-            box.addEventListener(
-                'click',
-                function(){
-                    box.classList.add('cambio-colore');
-         } )
-         }
+            const creaTabellone = creaCasella('div', 'casella', i);
+            container.append(creaTabellone);
+        }
     }
+    container.classList.add('show')
 }
-// dichiarazione variabili 
-const x = 'div';
-const y = 'casella';
 let cellNumber = 0;
 const livDifficolta = document.getElementById('difficoltà');
-// richiamo funzione 
-creaCasella(x, y);
+
 
 const btnPlay = document.getElementById('btn-play');
 
-btnPlay.addEventListener('click', Tabellone)
+btnPlay.addEventListener('click', Tabellone);
